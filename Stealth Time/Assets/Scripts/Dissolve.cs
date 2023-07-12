b6d2cf2ea453ee6cae50c8d88ffdc1bb44112d7d
IDrSpacedI <hi189@student.anglia.ac.uk>
2023-07-12 14:53:05 +0100

    crouch slider

--- Stealth Time/Assets/Scripts/Dissolve.cs
+++ Stealth Time/Assets/Scripts/Dissolve.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dissolve : MonoBehaviour
 {
@@ -10,11 +11,20 @@ public class Dissolve : MonoBehaviour
     private bool isDissolving = false; // Flag to check if currently dissolving
     private float dissolveAmount = 0f; // Current dissolve amount
 
+    public Slider countdownSlider; // Reference to the UI slider
+    public float countdownDuration = 30f; // Duration of the countdown in seconds
+
+    private bool isCountingDown = false; // Flag to check if currently counting down
+    private float countdownTime = 0f; // Current countdown time
+
     private void Start()
     {
         // Set the initial dissolve amount to 0
         foreach (Material material in materials)
             material.SetFloat("_Dissolve", 0f);
+
+        // Set the initial value of the slider
+        countdownSlider.value = countdownSlider.maxValue;
     }
 
     private void Update()
@@ -27,6 +37,30 @@ public class Dissolve : MonoBehaviour
                 StartCoroutine(DissolveMaterials());
             else
                 StartCoroutine(ReverseDissolveMaterials());
+
+
+            if (!isCountingDown)
+            {
+                StartCountdown();
+            }
+            else
+            {
+                StopCountdown();
+            }
+        }
+
+        if (isCountingDown)
+        {
+            countdownTime -= Time.deltaTime;
+
+            if (countdownTime <= 0f)
+            {
+                StopCountdown();
+            }
+            else
+            {
+                countdownSlider.value = countdownTime;
+            }
         }
     }
 
@@ -54,5 +88,19 @@ public class Dissolve : MonoBehaviour
 
             yield return null;
         }
+    }
+
+    private void StartCountdown()
+    {
+        isCountingDown = true;
+        countdownTime = countdownDuration;
+        countdownSlider.maxValue = countdownDuration;
+        countdownSlider.value = countdownDuration;
+    }
+
+    private void StopCountdown()
+    {
+        isCountingDown = false;
+        countdownSlider.value = countdownSlider.maxValue;
     }
 }
